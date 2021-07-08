988a444555ca674946d54cf95581264f8d6a06ea
Maxine <maxinemeurer@yahoo.com>
2021-07-08 16:08:37 -0400

    navbar properly responds to screen resize, add in state to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import NavbarItems from './NavbarItems'
 import './Navbar.css'
 
-class Navbar extends React.Component {
-    render(){
-        return(
-            <div>
-                <nav>
-                    <div className="logo">Logo</div>
-                        {NavbarItems.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    <Link className={item.cName} to={item.url}>
-                                        {item.title}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                </nav>
-            </div>
-        )
-    }
+function Navbar() {
+
+const [open, setOpen] = useState(false)
+    return(
+        <div>
+            <nav>
+                <div className="logo">Logo</div>
+                <ul className="nav-links" style={{transform: open ? "translateX(0px)" : ""}}>
+                    {NavbarItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to={item.url} style={{transform: open ? "translateX(0px)" : ""}}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                            //  <li>
+                            // <Link to="/">Home</Link>
+                            // </li> 
+                        )
+                    })}
+                    </ul>
+                    <i onClick={() => setOpen(!open)} className="fas fa-bars burger"></i>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
