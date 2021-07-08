6b3b947b18d9a4c114967af1c2dd1cfc847d9f8d
Maxine <maxinemeurer@yahoo.com>
2021-07-08 13:46:06 -0400

    add links to nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import About from './components/About';
+import Contact from './components/Contact'
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Maxine Meurer</h1>
-      < Navbar/>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <Router >
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import NavbarItems from './NavbarItems'
+import './Navbar.css'
 
 class Navbar extends React.Component {
     render(){
         return(
-            <p>text</p>
+            <div>
+                <nav>
+                    <div className="logo">Logo</div>
+                        {NavbarItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link className={item.cName} to={item.url}>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                </nav>
+            </div>
         )
     }
 }
