fa9af9bb40b1bf780dd891f1a9c4be353efa881a
Maxine <maxinemeurer@yahoo.com>
2021-07-09 23:26:58 -0400

    navbar styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,31 @@ import React from 'react'
 class About extends React.Component {
     render(){
         return(
-            <p>about</p>
+            <div>
+                <h3>Hi!</h3>
+                <p> 
+                    I'm Maxine, a recent sofware engineering graduate of Flatiron School. 
+                </p>
+                <p>
+                    My journey as a programmer began my last semester of college when I decided to take 
+                    a Java and Python class for fun. Come around end of semester, I was planning out which languages I wanted to learn next. 
+                </p>
+                <p>
+                    As an environmental sciences/engineering major, and STEM nerd,
+                    coding always sparked an interest in me. And, if there's one thing I regret, it's 
+                    not starting it sooner. But, in the interim, I recieved a ton of priceless life experience.
+                </p>
+                <p>
+                    Prior to my life of watching tech talks, while I was still completing my degree fulltime, I became a mother. As many know, being a 
+                    parent is an every day lesson and challenge in itself, so if that wasn't enough, I also became a 
+                    head pre-school teacher. Tackling all three of these tasks simutanously taught me more than you could imagine, 
+                    and ultimately made me into the problem solving programmer I am today. 
+                </p>
+                <p>
+                    With a love for, and background, in humanitarian and environmental studies, 
+                    I hope to utilize programming in ways to make our world a better place.
+                </p>
+            </div>
         )
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import React from 'react'
 class Home extends React.Component {
     render(){
         return(
-            <p>text</p>
+            <div>
+                <h2> Hi there! My name is</h2>
+                <h1>Maxine Meurer</h1>
+                <h2>I'm a Fullstack Software Engineer</h2>
+                <h3>I have experience in Ruby on Rails, Python, JavaScript, React, Redux, and more.</h3>
+            </div>
         )
     }
 }
