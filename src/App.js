fb5e7262cee671fb1687fbe8e0b8bb15ee033e9c
Maxine <maxinemeurer@yahoo.com>
2021-07-16 16:19:21 -0400

    add switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home';
 import Projects from './components/Projects';
@@ -36,11 +36,13 @@ class App extends React.Component {
       <div>
         <Router >
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/resume" component={this.renderResumeContainer} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} /> 
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/resume" component={this.renderResumeContainer} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} /> 
+          </Switch>
         </Router>
       {/* < Sidebar /> */}
       </div>
