ae49833cb26563d3267ebdd84a9a16417a641b9e
Maxine <maxinemeurer@yahoo.com>
2021-07-16 16:24:59 -0400

    add switch

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Router basename="/">
+        <Router>
           <Navbar />
           <Switch>
             <Route path="/" component={Home} />
