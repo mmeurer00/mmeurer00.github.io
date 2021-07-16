39661acc559cfcb271586c664612b0f7e52c5a8a
Maxine <maxinemeurer@yahoo.com>
2021-07-16 17:11:14 -0400

    fix nav

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ class App extends React.Component {
                 <Navbar />
                   <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/resume" component={this.renderResumeContainer} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" component={Contact} /> 
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/resume" component={this.renderResumeContainer} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} /> 
                   </Switch>
               </Fragment>
         </Router>
