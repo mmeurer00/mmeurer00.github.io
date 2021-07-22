219202788befb4149ef9d4278246a89c683e53f1
Maxine <maxinemeurer@yahoo.com>
2021-07-22 10:24:31 -0400

    alter link for linkedin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
             <a href="https://github.com/mmeurer00" rel="noopener noreferrer" target="_blank" >
             <img src={("https://raw.githubusercontent.com/iona-b/iona-b.github.io/cb12063880c43ccb7aec720b4df6e712c0c3498f/src/media/github-icon.svg")} alt='Github' className="contact-icons" />
             </a>
-            <a href="https://www.linkedin.com/in/maxine-m-b405a4154" rel="noopener noreferrer" target="_blank" >
+            <a href="https://www.linkedin.com/in/maxinemeurer/" rel="noopener noreferrer" target="_blank" >
             <img src={("https://raw.githubusercontent.com/iona-b/iona-b.github.io/cb12063880c43ccb7aec720b4df6e712c0c3498f/src/media/linkedin-icon.svg")} alt='LinkedIn' className="contact-icons" />
             </a>
             <a href="https://dev.to/mmeurer00" rel="noopener noreferrer" target="_blank" >
