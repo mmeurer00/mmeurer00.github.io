1a5d473945a6ce308d510b82aa10e561efe4737f
Maxine <maxinemeurer@yahoo.com>
2021-08-11 12:41:05 -0400

    finish logo cube

--- src/components/LogoCube/LogoCube.js
+++ src/components/LogoCube/LogoCube.js
@@ -15,23 +15,23 @@ class LogoCube extends React.Component {
                 }}
             >
             <Cube size={300} index="front">
-                <div>front
-                    <img src="https://zhenyong.github.io/react/img/logo.svg"></img>
+                <div style={{backgroundColor: '#222'}}>
+                    <img style={{width: '280px', height: '300px'}} src="https://zhenyong.github.io/react/img/logo.svg"></img>
                 </div>
-                <div>right
-                    <img src="https://wp.sitepen.com/wp-content/uploads/2016/04/js.png"></img>
+                <div style={{backgroundColor: '#f7df32'}}>
+                    <img style={{width: '280px', height: '300px'}} src="https://wp.sitepen.com/wp-content/uploads/2016/04/js.png"></img>
                 </div>
-                <div>back
-                    <img src="https://miguel-savignano.gallerycdn.vsassets.io/extensions/miguel-savignano/ruby-symbols/0.1.8/1561456903376/Microsoft.VisualStudio.Services.Icons.Default"></img>
+                <div style={{backgroundColor: 'white'}}>
+                    <img style={{width: '280px', height: '300px'}} src="https://miguel-savignano.gallerycdn.vsassets.io/extensions/miguel-savignano/ruby-symbols/0.1.8/1561456903376/Microsoft.VisualStudio.Services.Icons.Default"></img>
                 </div>
-                <div>left
-                <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/java-4-logo.png"></img>
+                <div style={{backgroundColor: 'white'}}>
+                <img style={{width: '280px', height: '300px'}} src="https://storage.needpix.com/rsynced_images/logo-2582747_1280.png"></img>
                 </div>
-                <div style={{backgroundColor: "white"}}>top
-                <img src=""></img>
+                <div style={{backgroundColor: 'white'}}>
+                <img style={{width: '280px', height: '300px'}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1280px-HTML5_logo_and_wordmark.svg.png"></img>
                 </div>
-                <div>bottom
-                <img src=""></img>
+                <div style={{backgroundColor: 'white'}}>
+                <img style={{width: '280px', height: '300px'}} src="https://www.logolynx.com/images/logolynx/18/18a311c9932a4327ded4436577bda857.png"></img>
                 </div>
             </Cube>
             </div>
